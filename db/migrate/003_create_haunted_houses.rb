9f28260dfe8f6e4d1b46b79458748346de2c7173
AlecGrey <alecgrey.coding@gmail.com>
2020-10-20 08:12:56 -0700

    migrated and created model classes

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -11,6 +11,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[6.0]
             t.datetime :opening_date
             t.datetime :closing_date
             t.text :description
+        end
     end
 
 end
